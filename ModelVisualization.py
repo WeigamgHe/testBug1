4641994eb70f4d85d5fe27d8a1513143f3c94c76
何维刚 <2906378463@qq.com>
2022-08-18 23:44:15 +0800

    更新了模型可视化的代码

--- ModelVisualization.py
+++ ModelVisualization.py
@@ -1,86 +1,87 @@
 import os.path
 import sys
 import torch
-# import poppler
+import json
+import createModel as cm
+import argparse
+import hiddenlayer as hl
 import base_net
+import torchvision.models as md
 from pdf2image import convert_from_path
 from torchviz import make_dot
 from grad_cam import __model__
+import torchvision.models
+from base_net import *
 
+import grad_cam as gc
 sys.path.append(r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test\grad_cam')
 sys.path.append(r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test\grad_cam'
                 r'\ModelVisualizationImageRes')
 sys.path.append(r'C:\Users\axin\anaconda3\Lib\site-packages\torchviz')
 
+dir = r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test\grad_cam' \
+      r'\ModelVisualizationImageRes'
 
-class ConNet(torch.nn.Module):
-    def __init__(self):
-        super(ConNet, self).__init__()
-        self.conv1 = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 16, 3, 1, 1),
-            torch.nn.ReLU(),
-            torch.nn.AvgPool2d(2, 2)
-        )
-        self.conv2 = torch.nn.Sequential(
-            torch.nn.Conv2d(16, 32, 3, 1, 1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2)
-        )
-        self.fc = torch.nn.Sequential(
-            torch.nn.Linear(32 * 7 * 7, 128),
-            torch.nn.ReLU(),
-            torch.nn.Linear(128, 64),
-            torch.nn.ReLU()
-        )
-        self.out = torch.nn.Linear(64, 10)
+lib__bin = r'C:\Users\axin\anaconda3\Lib\site-packages\poppler-0.67.0_x86\poppler-0.67.0\bin'
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc(x)
-        output = self.out(x)
-        return output
+outputfile_path = r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test' \
+                  r'\grad_cam\ModelVisualizationImageRes'
 
+parser = argparse.ArgumentParser(description='create Model Train.')
 
-def getParameter():
-    global dir, lib__bin, outputfile_path
-    dir = r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test\grad_cam' \
-          r'\ModelVisualizationImageRes'
-    lib__bin = r'C:\Users\axin\anaconda3\Lib\site-packages\poppler-0.67.0_x86\poppler-0.67.0\bin'
-    outputfile_path = r'C:\Users\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\Test' \
-                      r'\grad_cam\ModelVisualizationImageRes'
+parser.add_argument('--config',
+                    default=r'C:\Users\\axin\Desktop\DL\cam_draw\deep-learning-for-image-processing\pytorch_classification\grad_cam\config.json')
 
+class load_json():
+    def __init__(self,json):
+        """
+        json_path 是config.json的路径！！
+        """
+        self.outputPath = None
+        self.KernalVisualizationConfig = None
+        self.model = None
+        self.datasetConfig = None
+        self.dataset = None
+        self.json_path = json
+        self.parse_json(json)
+
+    def parse_json(self, json_path):
+        """
+        读取config.json来初始化类的json信息
+        """
+        print('\n\n json_path = ', json_path)
+        with open(json_path, "r", encoding="utf-8") as fr:
+            params = json.load(fr)
+
+        self.initMember(params)
+
+    def initMember(self, params):
+        """
+        初始化类成员
+        """
+        self.dataset = params.get("dataset")
+        self.datasetConfig = params.get("datasetConfig")
+        self.model = params.get("model")
+        self.KernalVisualizationConfig = params.get("KernalVisualizationConfig")
+        self.outputPath = params.get("outputPath")
+
+    def load(self):
+        """
+        name 是模型的名字
+        返回一个json指定的模型model
+        """
+        pth_files = self.model.get("modelStructurePath")
+        model = __model__.get(self.model.get("name"))(pretrained=True)
+        model.load_state_dict(torch.load(pth_files))
+        return model
 
 if __name__ == '__main__':
-    # convnet = ConNet()
-    # x = torch.randn(size=(1, 1, 28, 28))
-    # y = convnet(x)
+    args = parser.parse_args()
+    obj = load_json(args.config)
+    model = obj.load()
+    hl_graph = hl.build_graph(model, torch.zeros([3, 3, 224, 224]))
+    hl_graph.theme = hl.graph.THEMES['blue'].copy()
+    hl_graph.save('./ModelVisualizationImageRes/Resnet18.png', format='png')
+    print('Have done!')
 
-    # conv_viz = make_dot(y, params=dict(list(convnet.named_parameters()) + [("x", x)]))
-    #
-    # conv_viz.format = "png"
-    #
-    # conv_viz_directory = "data"
-    # conv_viz.view()
-    getParameter()
 
-    model_name = "AlexNet"
-    x = torch.rand(8, 3, 256, 512)
-    model = base_net.AlexNet()
-    # model = __model__.get(model_name)
-
-    y = model(x)
-    g = make_dot(y)
-
-    book_path = os.path.join(str(dir), str(model_name + "_ok"))
-    g.render(book_path, view=False)
-    book = os.path.join(dir, str(model_name + "_ok.pdf"))
-
-    pages = convert_from_path(book, dpi=600,
-                              poppler_path=lib__bin,
-                              output_file=outputfile_path)
-
-    img_path = os.path.join(dir ,'res.jpeg')
-    for page in pages:
-        page.save(img_path)
